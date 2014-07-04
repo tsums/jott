38b1bf1d68ebd280f079fed4360e7872d22b80d6
tsums <tresummerfield@gmail.com>
2014-07-04 15:46:20 -0400

    Added some arg parsing

--- jott.go
+++ jott.go
@@ -1,7 +1,33 @@
+/*
+    jott - a terminal note taking program
+    Trevor Summerfield
+    http://trevorsummerfield.com
+
+*/
+
 package main
 
 import "fmt"
+import "os"
+
+func syntax() {
+    fmt.Println("This is the syntax error message!")
+}
 
 func main() {
-    fmt.Printf("This is the jott program!\n")
+
+    args := os.Args[1:]
+
+    if len(args) == 0 {
+        syntax()
+        return
+    }
+
+
+
+    if args[0] == "-n" || args[0] == "new"{
+        fmt.Println("Making a new jott!")
+    } else {
+        syntax()
+    }
 }
