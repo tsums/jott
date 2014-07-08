a3ad8358365e82bc5fa5a38c6c9c6ba0b7d6cbad
tsums <tresummerfield@gmail.com>
2014-07-08 15:19:56 -0400

    More Verbosity

--- jott.go
+++ jott.go
@@ -36,11 +36,16 @@ func msToTime(ms string) (time.Time, error) {
 
 // this function gets called whenever we can't parse user syntax
 func syntax() {
-    fmt.Println("This is the syntax error message!\nIn the future, this should be useful.")
+    fmt.Println("jott - quick notes for your terminal - v0.2 \"itworks\"")
+}
+
+func params() {
+    fmt.Print("n | new\t\tadd a new jott to this user's store\n" + "ls | list\tlist jotts in this user's store\n" + "purge\t\tremove all jotts from this user's store.\n")
 }
 // creates new jotts and adds them to the store
 func new(text []string) {
     if (len(text) <= 0) {
+        fmt.Println("jott:\t enter new jott below, [CTRL+D] to save.")
         scanner := bufio.NewScanner(os.Stdin)
         var line string
         for scanner.Scan() {
@@ -91,6 +96,9 @@ func list(num int) {
         fmt.Printf("\t")
 		fmt.Println(readBack["text"])
 	}
+    if len(queryResult) == 0 {
+        fmt.Println("jott:\tno jotts found.")
+    }
 
 }
 
@@ -113,20 +121,20 @@ func makeDB() {
 
 func main() {
 
-    dbHome = "/tmp/jott"
+    dbHome = os.Getenv("HOME") + "/.jott"
     // check to see if there is a db here, make one if not
     if _, err := os.Stat(dbHome); err != nil {
-        fmt.Println("init: Database not Found.\ninit: Making .jott file at current wrkdir")
+        fmt.Println("init: Database not Found.\ninit: Making .jott file at: " + dbHome)
         makeDB()
         os.Exit(0)
     }
-    // if there was, open it
 
     args := os.Args[1:]
 
     // if no args, throw syntax message
     if len(args) == 0 {
         syntax()
+        fmt.Println("run 'jott help' for usage information.")
         os.Exit(1)
     }
 
@@ -143,18 +151,10 @@ func main() {
             fmt.Println("not purging jotts")
             os.Exit(0)
         }
-    }
-
-    //new stuff since last commit
-
-    // if given new flag, check to see if we were given the jott
-    if args[0] == "n" || args[0] == "new" {
-        if (len(args) > 1) {
-            new(args[1:len(args)])
-        } else {
-            new([]string{})
-        }
-    // if given list flag, check to see if we were given the num
+    } else if args[0] == "help" || args[0] == "h" {
+        syntax()
+        params()
+        os.Exit(0)
     } else if (args[0] == "ls" || args[0] == "list") {
         if len(args) == 2 {
             num, _ := strconv.Atoi(args[1])
@@ -162,9 +162,13 @@ func main() {
         } else {
             list(5)
         }
-    // we were given args, but they didn't mean anything
-    // throw syntax message and quit
+    } else if args[0] == "n" || args[0] == "new" {
+        if (len(args) > 1) {
+            new(args[1:len(args)])
+        } else {
+            new([]string{})
+        }
     } else {
-        syntax()
+        fmt.Println("jott:\tcommand not recognized: run 'jott help' for syntax.")
     }
 }
