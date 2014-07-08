1b6edf963a60d0d96f85d14a73357dbf775dc924
tsums <tresummerfield@gmail.com>
2014-07-08 19:18:25 -0400

    Cleaned formatting, more verbose comments.

--- jott.go
+++ jott.go
@@ -8,6 +8,7 @@
 
 package main
 
+// imports, man
 import (
     "encoding/json"
     "fmt"
@@ -21,20 +22,11 @@ import (
 
 // constants and vars and stuff
 
-var dbHome string
-var jDB db.DB
+var dbHome string // global db location for loading
+var jDB db.DB // global access to the db once loaded.
 
+// functions, man
 
-func msToTime(ms string) (time.Time, error) {
-    msInt, err := strconv.ParseInt(ms, 10, 64)
-    if err != nil {
-        return time.Time{}, err
-    }
-
-    return time.Unix(0, msInt*int64(time.Millisecond)), nil
-}
-
-// this function gets called whenever we can't parse user syntax
 func syntax() {
     fmt.Println("jott - quick notes for your terminal - v0.2 \"itworks\"")
 }
@@ -42,8 +34,9 @@ func syntax() {
 func params() {
     fmt.Print("n | new\t\tadd a new jott to this user's store\n" + "ls | list\tlist jotts in this user's store\n" + "purge\t\tremove all jotts from this user's store.\n")
 }
-// creates new jotts and adds them to the store
+
 func new(text []string) {
+    // If not given text in args, ask for it from stdin
     if (len(text) <= 0) {
         fmt.Println("jott:\t enter new jott below, [CTRL+D] to save.")
         scanner := bufio.NewScanner(os.Stdin)
@@ -53,12 +46,13 @@ func new(text []string) {
             text = append(text, line)
         }
     }
-
+    // open the database
     jDB , err := db.OpenDB(dbHome)
     if err != nil {
         panic(err)
     }
-
+    // join together all of the input text
+    // quit if nothing.
     fText := strings.Join(text, " ")
     if len(fText) == 0 {
         fmt.Println("jott:\tNothing entered, quitting.")
@@ -66,36 +60,44 @@ func new(text []string) {
     }
 
     jotts := jDB.Use("jotts")
-
-    jotts.Insert(map[string]interface{}{"timestamp": int(time.Now().Unix()),"text": fText})
+    // insert the text with a unix timestamp.
+    jotts.Insert(map[string]interface{}{"timestamp": strconv.Itoa(int(time.Now().Unix())),"text": fText})
+    fmt.Println(strconv.Itoa(int(time.Now().Unix())))
     fmt.Println("jott:\t jott stored.")
 
 }
 
 func list(num int) {
+    // open the database
     jDB , err := db.OpenDB(dbHome)
     if err != nil {
         panic(err)
     }
     jotts := jDB.Use("jotts")
+
+    // query for the jotts, this query is rudimentary
     var query interface{}
     json.Unmarshal([]byte(`[{"has": ["timestamp"]}]`), &query)
-
-    queryResult := make(map[int]struct{}) // query result (document IDs) goes into map keys
-
+    // run the query, result is doc keys in a map.
+    queryResult := make(map[int]struct{})
     if err := db.EvalQuery(query, jotts, &queryResult); err != nil {
         panic(err)
     }
 
+    // iterate over jotts , printing them *nicely*
     for id := range queryResult {
 		readBack, err := jotts.Read(id)
 		if err != nil {
 			panic(err)
 		}
+
+//        s := readBack["timestamp"].(int64)
+
         fmt.Print(readBack["timestamp"])
         fmt.Printf("\t")
 		fmt.Println(readBack["text"])
 	}
+    // if no jotts, let the user know.
     if len(queryResult) == 0 {
         fmt.Println("jott:\tno jotts found.")
     }
@@ -109,10 +111,11 @@ func makeDB() {
     if (err != nil) {
         panic(err)
     }
+    // make the jotts collection.
     if err := jDB.Create("jotts"); err != nil {
         panic(err)
     }
-
+    // index the collection based on timestamp.
     jotts := jDB.Use("jotts")
     if err := jotts.Index([]string{"timestamp"}); err != nil {
         panic(err)
@@ -120,7 +123,7 @@ func makeDB() {
 }
 
 func main() {
-
+    // we put the database in the user's home folder
     dbHome = os.Getenv("HOME") + "/.jott"
     // check to see if there is a db here, make one if not
     if _, err := os.Stat(dbHome); err != nil {
